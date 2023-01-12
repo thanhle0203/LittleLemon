3a32e0fd1bc0b53b8ac1572fac6d5ad9e2fe24c8
Thanh Le <thanhlevan0203@gmail.com>
2023-01-12 00:00:52 -0800

    Create User, Subject, Teacher model

--- LittleLemon/myproject/settings.py
+++ LittleLemon/myproject/settings.py
@@ -77,7 +77,7 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
         "OPTIONS": {
-            'read_default_file': 'etc/mysql/my.cnf',
+            'read_default_file': '/etc/mysql/my.cnf',
         },
     }
 }
